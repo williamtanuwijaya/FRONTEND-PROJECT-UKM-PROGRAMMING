9722831336a83115abcc7dafb7a469f4f8c9d627
ChristoferEvan <117826706+ChristoferEvan@users.noreply.github.com>
2024-04-16 22:01:50 +0700

    Result Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:bubadibako/screens/landing_screen.dart';
+import 'package:bubadibako/screens/result_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LandingScreen(),
+      home: ResultScreen(),
     );
   }
 }
